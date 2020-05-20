a262ab8327523f322299c868381de95729bcfdf4
udayk <udaykumar1113@gmail.com>
2020-05-20 09:02:10 -0400

    Added spring security dependency

---
+++ src/main/java/com/uday/springbootsecurity/HomeResource.java
@@ -0,0 +1,12 @@
+package com.uday.springbootsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+}
