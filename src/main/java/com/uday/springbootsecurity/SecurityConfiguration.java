f2115cfa20e956cd9482fe4f08295702adbc3d5e
udayk <udaykumar1113@gmail.com>
2020-05-21 09:01:29 -0400

    In memory authentication

---
+++ src/main/java/com/uday/springbootsecurity/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.uday.springbootsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("foo")
+                .password("foo")
+                .roles("USER")
+                .and()
+                .withUser("foo1")
+                .password("foo1")
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
